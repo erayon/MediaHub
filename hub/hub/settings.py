eab95f4372e5d5d6483c67878ce3c249f8c3c443
erayon <ayon.mi1@iiitmk.ac.in>
2018-04-17 15:17:31 +0530

    update

--- hub/hub/settings.py
+++ hub/hub/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'myapp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
