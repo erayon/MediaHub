c55c34c4a52a9f7a2cfcb04ca836a846cd084101
erayon <ayon.mi1@iiitmk.ac.in>
2018-04-18 15:30:28 +0530

    update

--- hub/myapp/views.py
+++ hub/myapp/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
-
+import pandas as pd
+import numpy as np
 # Create your views here.
 def home(request):
-    return render(request, 'myapp/home.html', {})
+	df = pd.read_pickle("/home/putus/working_directory/personal/movieParser/MediaHub/Database/movies_details.pckl")
+	name   = np.array(df['Movie_name'])
+	MId    = np.array(df['ID'])
+	rating = np.array(df['Rating'])
+	pic    = np.array(df['CoverPic'])
+	gen    = np.array(df['Generic'])
+	year   = np.array(df['Year'])
+	path   = np.array(df['path'])
+	rng    = len(df)
+	itemsz = []
+	for i in range(len(df)):
+	    #print(i)
+	    # dict == {}
+	    # you just don't have to quote the keys
+	    an_item = dict(Mname=name[i],Mid=MId[i],Mrating=rating[i],Mpic=pic[i],Mgen=gen[i],Myear=year[i],Mpath=path[i])
+	    itemsz.append(an_item)
+	return render(request, 'myapp/index.html', {'items':itemsz})
